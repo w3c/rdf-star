af4b6175ab4b26d3544f44c34dd99552f40fd4a6
Gregg Kellogg <gregg@greggkellogg.net>
2021-01-09 13:00:02 -0800

    Add make-jsonld-manifests.rb script, but use it just for creating nt/syntax and turtle/eval manifests due to potential formatting considerations.

---
+++ tests/make-jsonld-manifests.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'json/ld'
+require 'rdf/turtle'
+
+man_dir = File.expand_path("")
+%w{
+  nt/syntax
+  semantics
+  sparql/syntax
+  turtle/syntax
+  turtle/eval
+}.map {|p| File.expand_path(p)}.
+  each do |dir|
+  Dir.chdir(dir) do |path|
+    RDF::Turtle::Reader.open('manifest.ttl', base_uri: path) do |ttl_reader|
+      JSON::LD::Writer.open('manifest.jsonld',
+                            frame: "#{man_dir}/manifest-context.jsonld",
+                            context: "#{man_dir}/manifest-context.jsonld",
+                            base_uri: path
+      ) do |jsonld_writer|
+        puts "Create #{path}/manifest.jsonld"
+        jsonld_writer << ttl_reader
+      end
+    end
+  end
+end
+

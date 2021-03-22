facab5bf3b0d1c94b21e9b277dfa46d948a0e5e0
Andy Seaborne <andy@apache.org>
2021-03-22 21:29:30 +0000

    manifest.ttl - no BASE; PREFIX trs (#133)
    * manifest.ttl - no BASE; PREFIX trs
    
    * Adds some tests which were missing in N-Triples entries and an error in a Semantics entry.
    
    * Update script for generating JSON-LD manifests from Turtle manifests. Includes an isomorphism check that the manifests are, in fact, equivalent.
    
    Co-authored-by: Gregg Kellogg <gregg@greggkellogg.net>

--- tests/make-jsonld-manifests.rb
+++ tests/make-jsonld-manifests.rb
@@ -2,6 +2,9 @@
 require "bundler/setup"
 require 'json/ld'
 require 'rdf/turtle'
+require 'rdf/isomorphic'
+require 'rdf/ordered_repo'
+require 'rdf/normalize'
 
 man_dir = File.expand_path("")
 local_ctx = JSON.parse(File.read("#{man_dir}/manifest-context.jsonld"))
@@ -14,17 +17,23 @@ local_ctx = JSON.parse(File.read("#{man_dir}/manifest-context.jsonld"))
   turtle/eval
 }.each do |path|
   dir = File.expand_path(path)
-  base = "https://w3c.github.io/rdf-star/tests/#{path}/manifest"
-  RDF::Turtle::Reader.open("#{dir}/manifest.ttl") do |ttl_reader|
-    local_ctx['@context']['@base'] = base
-    JSON::LD::Writer.open("#{dir}/manifest.jsonld",
-                          frame: local_ctx,
-                          context: local_ctx,
-                          base_uri: base
-    ) do |jsonld_writer|
-      puts "#{dir}/manifest.jsonld"
-      jsonld_writer << ttl_reader
-    end
+  base = "https://w3c.github.io/rdf-star/tests/#{path}/"
+  ttl_graph = RDF::OrderedRepo.load("#{dir}/manifest.ttl", base_uri: base)
+  local_ctx['@context']['@base'] = base
+  local_ctx['@context']['trs'] = "https://w3c.github.io/rdf-star/tests/#{path}#"
+  JSON::LD::Writer.open("#{dir}/manifest.jsonld",
+    format: :jsonld, 
+    frame: local_ctx,
+    context: local_ctx,
+    base_uri: base) {|writer| writer << ttl_graph}
+
+  # Validate that the two graphs say the same thing.
+  jsonld_graph = RDF::OrderedRepo.load("#{dir}/manifest.jsonld", base_uri: base)
+  if !jsonld_graph.isomorphic?(ttl_graph)
+    STDERR.puts "expected #{dir}/manifest.jsonld to be isomorphic with expected #{dir}/manifest.ttl"
+    STDERR.puts "\nFrom Turtle:\n#{ttl_graph.dump(:normalize)}"
+    STDERR.puts "\nFrom JSON-LD:\n#{jsonld_graph.dump(:normalize)}"
+    exit(1)
   end
 end
 
